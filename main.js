ff191731573ab63a24e27d095ee85ffdfb93cd61
Sweatyg <fluuvys123@gmail.com>
2024-01-25 12:11:03 +0700

    UPDATED IMAGES AND ADDED LINKS

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-window.addEventListener(("scroll"),()=>{
-    document.querySelector("#Home h1").style.marginTop = `${window.scrollY * 1.5}px`
-    document.querySelector("#Mountain1").style.marginBottom = `${106 - window.scrollY}px`
-    document.querySelector("#leftCloud").style.marginLeft = `-${window.scrollY}px`
-    document.querySelector("#mainCloud").style.marginTop = `-${window.scrollY}px`
-    document.querySelector("#rightCloud").style.marginRight = `-${window.scrollY}px`
-})
-
-document.addEventListener("DOMContentLoaded", function () {
-    var aboutSection = document.getElementById("About");
-    var aboutNavLink = document.querySelector('a[href="#About"]');
-  
-    aboutNavLink.addEventListener("click", function (event) {
-      event.preventDefault();
-      aboutSection.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest", speed: "slow" });
-    });
-});
-
-function flipImage() {
-  document.querySelector('.front-image').style.transform = 'rotateY(-180deg)';
-  document.querySelector('.back-image').style.transform = 'rotateY(0deg)';
-}
-
-function unflipImage() {
-  document.querySelector('.front-image').style.transform = 'rotateY(0deg)';
-  document.querySelector('.back-image').style.transform = 'rotateY(180deg)';
-}
-  
 function flipImage(element) {
   element.querySelector('.front-image').style.transform = 'rotateY(-180deg)';
   element.querySelector('.back-image').style.transform = 'rotateY(0deg)';
@@ -35,3 +7,24 @@ function unflipImage(element) {
   element.querySelector('.front-image').style.transform = 'rotateY(0deg)';
   element.querySelector('.back-image').style.transform = 'rotateY(180deg)';
 }
+
+function openEtsyLink(element) {
+  var etsyLink = element.getAttribute('data-etsy-link');
+  window.open(etsyLink, "_blank");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('.img-content').forEach(function(element) {
+      element.addEventListener("mouseover", function() {
+          flipImage(this);
+      });
+
+      element.addEventListener("mouseout", function() {
+          unflipImage(this);
+      });
+
+      element.addEventListener("click", function() {
+          openEtsyLink(this);
+      });
+  });
+});
