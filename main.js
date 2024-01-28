83aa2f0d5e3b05501c40acefa725840d89903956
Sweatyg <fluuvys123@gmail.com>
2024-01-28 16:34:30 +0700

    fixed scroling issue

--- main.js
+++ main.js
@@ -13,6 +13,14 @@ function openEtsyLink(element) {
   window.open(etsyLink, "_blank");
 }
 
+window.addEventListener(("scroll"),()=>{
+  document.querySelector("#Home h1").style.marginTop = `${window.scrollY * 1.5}px`
+  document.querySelector("#Mountain1").style.marginBottom = `${106 - window.scrollY}px`
+  document.querySelector("#leftCloud").style.marginLeft = `-${window.scrollY}px`
+  document.querySelector("#mainCloud").style.marginTop = `-${window.scrollY}px`
+  document.querySelector("#rightCloud").style.marginRight = `-${window.scrollY}px`
+})
+
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll('.img-content').forEach(function(element) {
       element.addEventListener("mouseover", function() {
