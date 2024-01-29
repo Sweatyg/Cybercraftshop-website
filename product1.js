851f76886536cd41052f0d35f0cec414ecab4d7c
Sweatyg <fluuvys123@gmail.com>
2024-01-29 12:32:46 +0700

    Added second product page and content updated

---
+++ product1.js
@@ -0,0 +1,30 @@
+function openEtsyLink(element) {
+    var etsyLink = element.getAttribute('data-etsy-link');
+    window.open(etsyLink, "_blank");
+  }
+function flipImage(element) {
+  element.querySelector('.front-image').style.transform = 'rotateY(-180deg)';
+  element.querySelector('.back-image').style.transform = 'rotateY(0deg)';
+}
+
+function unflipImage(element) {
+  element.querySelector('.front-image').style.transform = 'rotateY(0deg)';
+  element.querySelector('.back-image').style.transform = 'rotateY(180deg)';
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.img-content').forEach(function(element) {
+        element.addEventListener("mouseover", function() {
+            flipImage(this);
+        });
+  
+        element.addEventListener("mouseout", function() {
+            unflipImage(this);
+        });
+  
+        element.addEventListener("click", function() {
+            openEtsyLink(this);
+        });
+    });
+  });
+  
